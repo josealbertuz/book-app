f2c90a43fb085a5941ae716ffea2aa9b3e09d5ff
Jose Antonio Albértuz <josealbertuz@gmail.com>
2021-09-11 17:19:09 +0200

    Added BookSearchState and BookSearchResponse to types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,6 +5,90 @@ export type StackParamList = {
 
 export type BookResponse = {
     books: [],
+export type BookSearchState = {
+    books: BookListItemProps[],
+    isLoading: boolean,
+    error: boolean
+}
+
+export type BookSearchResponse = {
+    numFound:      number;
+    start:         number;
+    numFoundExact: boolean;
+    docs:          Doc[];
+    num_found:     number;
+    q:             string;
+    offset:        null;
+}
+
+export type Doc = {
+    key:                                   string;
+    text:                                  string[];
+    type:                                  string;
+    seed:                                  string[];
+    title:                                 string;
+    title_suggest:                         string;
+    has_fulltext:                          boolean;
+    edition_count:                         number;
+    edition_key:                           string[];
+    publish_date?:                         string[];
+    publish_year?:                         number[];
+    first_publish_year?:                   number;
+    lccn?:                                 string[];
+    publish_place?:                        string[];
+    oclc?:                                 string[];
+    contributor?:                          string[];
+    lcc?:                                  string[];
+    ddc?:                                  string[];
+    isbn?:                                 string[];
+    last_modified_i:                       number;
+    ebook_count_i:                         number;
+    ia?:                                   string[];
+    public_scan_b?:                        boolean;
+    ia_collection_s?:                      string;
+    lending_edition_s?:                    string;
+    lending_identifier_s?:                 string;
+    printdisabled_s?:                      string;
+    cover_edition_key?:                    string;
+    cover_i?:                              number;
+    publisher?:                            string[];
+    language?:                             string[];
+    author_key?:                           string[];
+    author_name?:                          string[];
+    author_alternative_name?:              string[];
+    person?:                               string[];
+    place?:                                string[];
+    subject?:                              string[];
+    time?:                                 string[];
+    id_alibris_id?:                        string[];
+    id_amazon?:                            string[];
+    id_canadian_national_library_archive?: string[];
+    id_goodreads?:                         string[];
+    id_google?:                            string[];
+    id_librarything?:                      string[];
+    id_overdrive?:                         string[];
+    id_paperback_swap?:                    string[];
+    id_wikidata?:                          string[];
+    ia_loaded_id?:                         string[];
+    ia_box_id?:                            string[];
+    id_dep_sito_legal?:                    string[];
+    publisher_facet?:                      string[];
+    person_key?:                           string[];
+    time_facet?:                           string[];
+    place_key?:                            string[];
+    person_facet?:                         string[];
+    subject_facet?:                        string[];
+    _version_:                             number;
+    place_facet?:                          string[];
+    lcc_sort?:                             string;
+    author_facet?:                         string[];
+    subject_key?:                          string[];
+    time_key?:                             string[];
+    ddc_sort?:                             string;
+    first_sentence?:                       string[];
+    subtitle?:                             string;
+    id_bcid?:                              string[];
+}
     isLoading: boolean,
     error: boolean
 }
